e650a39d4c27bb84ce82868eec4c36f17d55b2f7
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-01-21 14:54:51 +0000

    Regular update: 2020-01-21 14:54:50 UTC

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ window.app = {};
 
 window.allXhr = [];
 
+app.db = {};
+
 app.renderErr404 = function () {
     setTimeout(function () {
         app.setTitle('404 找不到页面');
@@ -16,21 +18,22 @@ app.renderErr404 = function () {
 };
 
 app.setTitle = function (title) {
-    document.title = `${title} — 三自飞面神教爱国会`
+    document.title = `${title} — 三自飞面神教爱国会`;
 };
 
 app.setHierarchyLocation = function (argv) {
     document.querySelector('#js-pageNavHierarchyLocation').innerHTML = `
-        <span><a href="/">首页</a></span> /
+        位置：<span><a href="/">首页</a></span> /
     ` + argv.map(function (arg) {
         var $sample_arg = [
             'link',
             'dangjian'
         ];
-        return ({
-            link: `<span><a href="/?/${arg[1]}"></a></span>`,
+        var tmp = ({
+            link: `<span><a href="/?/${arg[1]}/">${currentScene[1]}</a></span>`,
             label: `<span>${arg[1]}</span>`
-        })[arg[0]]
+        })[arg[0]];
+        return tmp;
     }).join(' / ');
 };
 
@@ -44,32 +47,78 @@ app.get = function (url, callback) {
     xhr.send();
 };
 
-app.scenes = {};
-
-app.sceneRenderers = {
+app.canvasRenderers = {
+    '_articleListItem': function (scene, entry) {
+        return `<div class="articles-list-item">
+            <a class="articles-list-item-inner" href="/?/${scene}/${entry.i}">
+                <div class="articles-list-item-inner-title">
+                    ${entry.t}
+                </div>
+                <div class="articles-list-item-inner-metadata">
+                    <span class="articles-list-item-inner-metadata-date">
+                        ${(new Date(entry.d)).toISOString().replace('T', ' ').replace(/\d{2}:\d{2}:\d{2}\.\d{3}Z/, '')}
+                    </span>
+                </div>
+            </a>
+        </div>`;
+    },
+    'article_detail': function (argv) {
+        console.log(argv);
+        app.setHierarchyLocation(argv.hierarchyLocation);
+        document.querySelector('#js-realSceneContent').innerHTML = `<div>
+            <div>
+                <h2>${app.db[argv.scene][argv.index].t}</h2>
+            </div>
+            <div style="padding: 0 0 30px;">
+                <div class="articles-list-item-inner-metadata">
+                    <div class="articles-list-item-inner-metadata-date">
+                        ${(new Date(app.db[argv.scene][argv.index].d)).toISOString().replace('T', ' ').replace(/\d{2}:\d{2}:\d{2}\.\d{3}Z/, '')}
+                    </div>
+                </div>
+            </div>
+            <div id="js-articleFileContent">
+            </div>
+        </div>`;
+        app.get(`/db/${argv.scene}/db/${argv.index}.html`, function (e) {
+            document.querySelector('#js-articleFileContent').innerHTML = e.target.responseText;
+        });
+    },
     'home': function () {},
     'dangjian': function () {
-        app.get('/db/dangjian/list.json', function (e) {
-            document.querySelector('#js-scene-dangjian-listOfArticles').innerHTML = JSON.parse(e.target.responseText).reverse().map(function (entry, index) {
+        app.setHierarchyLocation([['label', '党建工作']]);
+        app.renderInnerPage('dangjian');
+    },
+    'xinxi': function () {
+        app.setHierarchyLocation([['label', '信息公开']]);
+    },
+    'huodong': function () {
+        app.setHierarchyLocation([['label', '宗教活动']]);
+        app.renderInnerPage('huodong');
+    }
+};
+
+app.renderInnerPage = function (scene, isEinListPage) {
+    app.get(`/db/${scene}/list.json`, function (e) {
+        app.db[scene] = JSON.parse(e.target.responseText);
+        var newRegExp = new RegExp(`^\\?\\/${scene}\\/(\\d+)$`);
+        if (location.search.match(newRegExp)) {
+            var matcher = parseInt(location.search.match(newRegExp)[1]);
+            console.log('matcher');
+            app.canvasRenderers['article_detail']({
+                index: matcher,
+                scene: scene,
+                hierarchyLocation: [['link', scene],['label',app.db[scene][matcher].t]]
+            });
+        } else {
+            document.querySelector('#js-scene-listOfArticles').innerHTML = JSON.parse(e.target.responseText).reverse().map(function (entry, index) {
                 if (entry.t === '{{null}}') {
                     return '';
                 } else {
-                    return `<div class="articles-list-item">
-                        <a class="articles-list-item-inner" href="/?/dangjian/${entry.i}">
-                            <div class="articles-list-item-inner-title">
-                                ${entry.t}
-                            </div>
-                            <div class="articles-list-item-inner-metadata">
-                                <span class="articles-list-item-inner-metadata-date">
-                                    ${(new Date(entry.d)).toISOString().replace('T', ' ').replace(/\d{2}:\d{2}:\d{2}\.\d{3}Z/, '')}
-                                </span>
-                            </div>
-                        </a>
-                    </div>`;
+                    return app.canvasRenderers['_articleListItem'](scene, entry);
                 };
             }).join('');
-        });
-    }
+        };
+    });
 };
 
 app.parseScene = function () {
@@ -94,7 +143,7 @@ app.parseScene = function () {
             app.setTitle(currentScene[1]);
             app.get(`/scenes/${currentScene[0]}.html`, function (e) {
                 document.querySelector('#js-realSceneContent').innerHTML = e.target.responseText;
-                setTimeout(app.sceneRenderers[currentScene[0]], 50);
+                setTimeout(app.canvasRenderers[currentScene[0]], 50);
             });
         }
     };
